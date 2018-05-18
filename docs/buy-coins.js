4a2859d7cd3a54b57de6675473a1d4197792b7ca
Rene Kapusta <rene.kapusta@gmail.com>
2018-05-18 12:06:23 +0200

    fix css

--- docs/buy-coins.js
+++ docs/buy-coins.js
@@ -1,16 +1,15 @@
-function initBuyCoins() {
+function initInstaCoin26() {
     // alert("Init: InstaCoin26 ...");
-    // console.log("Init: InstaCoin26 ...");
 
     var $invite = $('.UIHeader__invite-friends'),
         $accountInfo = $('.UIHeader__account-infos'),
         $InstaCoin26 = $('<div id="instacoin26" class="UIHeader__account-infos">InstaCoin26</div>');
 
 
-    $invite.replace($InstaCoin26);
-
-    // $accountInfo.append($InstaCoin26);
-
+    $invite.html($InstaCoin26).attr('href', '#instacoin26');
+    $invite.attr('href', '#instacoin26').on('click', function(e) {
+        e.preventDefault();
+    });
 }
 
-initBuyCoins();
+initInstaCoin26();
