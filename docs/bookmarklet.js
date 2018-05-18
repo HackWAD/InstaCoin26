c9c1f09d67631e834436596156cc41fae9acbbbb
Rene Kapusta <rene.kapusta@gmail.com>
2018-05-18 11:14:25 +0200

    refactor

--- docs/bookmarklet.js
+++ docs/bookmarklet.js
@@ -2,6 +2,6 @@ function() {
     var head = document.getElementsByTagName("head")[0];
     var scan = document.createElement('script');
     scan.type = 'text/javascript';
-    scan.src = 'https://hackwad.github.io/InstaCoin26/buy-cryptos.js';
+    scan.src = 'https://hackwad.github.io/InstaCoin26/buy-coins.js';
     head.appendChild(scan)
 }
